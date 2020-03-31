e7a78e21dae81505919a63c9219223d16c71fae7
Maksym <belkomaksy@gmail.com>
2020-03-31 12:17:31 +0300

    After lesson

--- 04class.sql
+++ 04class.sql
@@ -13,17 +13,19 @@
  */
 
 -- вывести имя продукта и маржу по стоимости
--- отсортировать по убыванию маржи и имени продукта по возростанию (3мя способами)
+-- отсортировать по убыванию маржи и имени продукта по возростанию (4мя способами)
 -- 1/11
  SELECT
+ 	--product_id,
 	prod.product_name as name,
-	prod.list_price - prod.standard_cost as margin/*,
-	prod.list_price, prod.standard_cost*/
+	prod.list_price - prod.standard_cost as margin--,
+--	prod.list_price, prod.standard_cost
 FROM
 	db_laba.dbo.products prod
-ORDER BY 2 DESC, 1;
+--ORDER BY 2 DESC, 1;
 --ORDER BY prod.list_price - prod.standard_cost DESC, 1;
 --ORDER BY prod.list_price - prod.standard_cost DESC, prod.product_name;
+ORDER BY margin DESC, prod.product_name;
 
 -- вывести имя продукта и процент маржи по стоимости (2мя способами)
 -- отсортировать по имени продукта по возростанию
@@ -31,9 +33,10 @@ ORDER BY 2 DESC, 1;
 SELECT
 	prod.product_name as name,
 	--prod.list_price , prod.standard_cost,
-   (prod.list_price - prod.standard_cost) / prod.standard_cost * 100 as margin_to_cost
-   --,(prod.list_price / prod.standard_cost -1)  * 100 as margin_to_cost_2
-   --,CAST(CAST((prod.list_price / prod.standard_cost -1)  * 100 as decimal(10,2) ) as varchar) + '%' as margin_to_cost_3_proc
+   --(prod.list_price - prod.standard_cost) / prod.standard_cost * 100 as margin_to_cost
+   --,(prod.list_price / prod.standard_cost -1) * 100 as margin_to_cost_2
+   CAST(CAST((prod.list_price / prod.standard_cost -1)  * 100 as decimal(10,2) ) as varchar) + '%' as margin_to_cost_3_proc
+ --  CAST((prod.list_price / prod.standard_cost -1)  * 100 as decimal(10,2) )+ '%' as margin_to_cost_4_proc
 FROM
 	db_laba.dbo.products prod
 order by
@@ -70,17 +73,21 @@ SELECT
 	--,RIGHT ( emp.last_name, 3 )
 	--,SUBSTRING ( emp.last_name , 1 ,3 ) 
 	--,SUBSTRING ( emp.last_name , 3 ,3 )
+	--,reverse(emp.last_name)
+	--,reverse(SUBSTRING ( reverse (emp.last_name) , 1 ,3 ))
 	--,LEN(emp.last_name) 
 	--,LTRIM (emp.last_name)
 	--,RTRIM (emp.last_name)
 	--,TRIM (emp.last_name)
-	--,UPPER (emp.last_name)
-	--,LOWER (emp.last_name)
-	--,CHARINDEX('a', emp.last_name) --oracle instr
-	--,SUBSTRING ( emp.last_name , CHARINDEX('a', emp.last_name) ,3 )
-	--,REPLACE(emp.last_name, 'as','+')
+	,UPPER (emp.last_name)
+	,LOWER (emp.last_name)
+	,CHARINDEX('a', emp.last_name) --oracle instr
+	,SUBSTRING ( emp.last_name , CHARINDEX('ash', emp.last_name) ,3 )
+	,REPLACE(emp.last_name, 'as','+')
 from db_laba.dbo.employees emp
-where LEN(emp.last_name) > 7; 
+where LEN(emp.last_name) > 7
+order by 1 desc; 
+--Washington
 --Rivera
 
 -- 5/11
@@ -99,8 +106,9 @@ select  LTRIM (' last name   '),
 -- вывести объеденение продуктов и категорий (2мя способами)
 -- 6/11
  select
-	*
-	--p.*, pc.*
+	--*
+	pc.*,
+	p.*
 from
 	db_laba.dbo.products p,
 	db_laba.dbo.product_categories pc
@@ -122,13 +130,14 @@ inner join db_laba.dbo.product_categories pc on
  select
 	p.product_name,
 	p.standard_cost,
+	--category_id,
 	--p.category_id category_id_prod,
 	--pc.category_id category_id_cat,
 	pc.category_name
 from
 	db_laba.dbo.products p
-inner join db_laba.dbo.product_categories pc on
--- join db_laba.dbo.product_categories pc on
+--inner join db_laba.dbo.product_categories pc on
+ join db_laba.dbo.product_categories pc on
 	p.category_id = pc.category_id
 where
 	pc.category_name != 'CPU' --pc.category_name not in ('CPU') <> 
@@ -148,20 +157,23 @@ order by 1;
 	emp.first_name,
 	emp.last_name,
 	--ord.order_id,
-	--ord.status
-	--emp.job_title
+	--ord.status,
+	--emp.job_title,
 	COUNT(ord.order_id)
 from
 	db_laba.dbo.employees emp
-left join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
---left outer join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
- --join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
+--left join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
+left outer join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
+-- join db_laba.dbo.orders ord on emp.employee_id = ord.salesman_id
 where
 	emp.job_title LIKE '%sal%'
 GROUP BY
 	emp.employee_id,
 	emp.first_name,
 	emp.last_name
+	--ord.order_id
+	--ord.status,
+	--emp.job_title,
 order by 4 desc;
 
 --select * from db_laba.dbo.orders ord where ord.salesman_id = 60
@@ -173,17 +185,19 @@ order by 4 desc;
 -- 10/11
 select * from
 	db_laba.dbo.employees emp1;
+
 SELECT
 	emp1.first_name + ' ' + emp1.last_name as employee,
-	--'managed by ',
-	emp2.first_name + ' ' + emp2.last_name as manager ,
-	COALESCE(emp2.first_name + ' ' + emp2.last_name, 'Tommy Bailey') as manager_2
+	'managed by ',
+	--emp2.first_name + ' ' + emp2.last_name as manager --,
+	COALESCE(emp2.first_name + ' ' + emp2.last_name, 'Tommy Bailey!!!') as manager_2
 from
-	db_laba.dbo.employees emp1
-left join db_laba.dbo.employees emp2 on
+	db_laba.dbo.employees as emp1
+left join db_laba.dbo.employees as emp2 on
 --join db_laba.dbo.employees emp2 on
 	emp1.manager_id = emp2.employee_id
---	inner lfkgflkgjklfjglk
+--inner lfkgflkgjklfjglk  on 
+--fdjkghfdjg = kjsfkjgshfg
 order by 1,2;
 
 /*
@@ -200,7 +214,8 @@ order by 1,2;
 	ord.order_id
 from
 	db_laba.dbo.employees emp
-FULL OUTER join db_laba.dbo.orders ord on
+--FULL OUTER join db_laba.dbo.orders ord on
+FULL join db_laba.dbo.orders ord on
 	emp.employee_id = ord.salesman_id
 where
 	emp.job_title LIKE '%sal%';
